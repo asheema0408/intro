4e89922a3761c590ff1a71ffc2588bf7cd411b83
asheema0408 <asheemma@seznam.cz>
2023-12-14 16:41:55 +0100

    file add

---
+++ github.py
@@ -0,0 +1,22 @@
+import random
+
+nahodna_cifra = random.randint(1, 15)
+
+nahodne_cislo = 6
+
+if nahodna_cifra == 6:
+        print(str(nahodna_cifra)+" omg trefil to " + str(nahodne_cislo))
+
+if nahodna_cifra < 6:
+        print(str(nahodna_cifra) +" ojojoj je mensi " + str(nahodne_cislo))
+
+if nahodna_cifra > 6: 
+        print(str(nahodna_cifra) +" jeeeee je to vetsi nez " + str(nahodne_cislo))
+
+
+
+
+
+
+
+
